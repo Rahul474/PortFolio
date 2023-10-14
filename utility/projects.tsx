91d954bfd238b95c8f98301dda168b6fa75ce535
Rahul474 <arahul473@gmail.com>
2023-10-14 11:25:10 -0700

    data changes

--- utility/projects.tsx
+++ utility/projects.tsx
@@ -1,51 +1,40 @@
 const projectsInformation = [
+  
   {
     name: 'Gif Maker',
-    websiteLink: 'https://gifmaker.snapbit.in',
-    scourceLink: 'https://github.com/KakaShI06/gifmaker',
+    websiteLink: 'https://gif-maker-zeta.vercel.app/',
+    scourceLink: 'https://github.com/Rahul474/GifMaker',
     smallDescription: 'Convert Videos To Gifs In Seconds',
     projectPic:
-      'https://res.cloudinary.com/doql129vl/image/upload/v1694690128/gifmaker-ss_vcylqq.png',
+      'https://res.cloudinary.com/da1qp92zl/image/upload/v1697307754/Capture-gifmaker_pgmbuu.png',
     description:
       'Turn your favorite videos into shareable gifs with GifMaker. Our easy-to-use app lets you quickly convert videos to gifs without any hassle. Choose your favorite clip, select the start and end points, and let GifMaker do the rest. Share your newly created gifs with your friends and family on social media or messaging apps. With GifMaker, the possibilities are endless.',
     ready: true,
     techStack: ['Reactjs', 'Nextjs', 'ffmpeg'],
   },
   {
-    name: 'Reddit Reels Automator',
-    websiteLink: 'https://www.youtube.com/shorts/y3vFDS6K6TU',
-    scourceLink: 'https://github.com/KakaShI06/reddit_automation',
-    smallDescription: 'Reddit Automation to Create Reels or Youtube Shorts',
+    name: 'Crypto Tracker',
+    websiteLink: 'https://652aac816c9a3507790ed8fe--chic-lamington-5aaae9.netlify.app/',
+    scourceLink: 'https://github.com/Rahul474/Crypto',
+    smallDescription: 'Crypto Tracker',
     projectPic:
-      'https://res.cloudinary.com/doql129vl/image/upload/v1694712579/reel-automator_x64hed.png',
-    description:
-      "Elevate video content creation through Python automation. Utilize Selenium and praw for scraping Reddit's top posts and MoviePy for precise video editing. Adding voice through text-to-speech library called pyttsx3. This automation approach ensures visually appealing and narratively compelling video content.",
+      'https://res.cloudinary.com/da1qp92zl/image/upload/v1697307469/Capture-crypto_np6nqj.png',
+    description: 'Crypto Tracker to get the latest price on daily basis',
     ready: true,
-    techStack: ['Python', 'Moviepy', 'Selenium'],
+    techStack: ['Reactjs', 'RapidApi', 'Material-Ui'],
   },
   {
     name: 'Blog Website',
-    websiteLink: 'https://blogs.snapbit.in',
-    scourceLink: 'https://github.com/KakaShI06/blog-website-frontend',
+    websiteLink: 'https://blog-l1eq.vercel.app/',
+    scourceLink: 'https://github.com/Rahul474/Blog',
     smallDescription: 'Personalised Blog Website',
     projectPic:
-      'https://res.cloudinary.com/doql129vl/image/upload/v1696262217/Screenshot_from_2023-10-02_21-21-15_ezmqzz.png',
+      'https://res.cloudinary.com/da1qp92zl/image/upload/v1697307792/Capture-blog_fxrcoh.png',
     description:
       'Just a personalised blog website where I post programming stuff there,',
     ready: true,
     techStack: ['Nextjs', 'Reactjs', 'Markdown'],
-  },
-  {
-    name: 'Youtube Clone',
-    websiteLink: 'https://fastidious-brigadeiros-f6f4ec.netlify.app/',
-    scourceLink: '',
-    smallDescription: 'You-Tube clone',
-    projectPic:
-      'https://res.cloudinary.com/doql129vl/image/upload/v1694713155/you-tube-player_dpyjhk.png',
-    description: 'Created a youtube clone using Reactjs and material-ui',
-    ready: true,
-    techStack: ['Reactjs', 'RapidApi', 'Material-Ui'],
-  },
+  }
 ]
 
 export default projectsInformation
